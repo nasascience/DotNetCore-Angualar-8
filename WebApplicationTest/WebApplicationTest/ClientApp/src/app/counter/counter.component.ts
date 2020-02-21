6bf2934aee24d64806297fa267e5bfe7264e1d74
Johan Perez <nasa_science@hotmail.com>
2020-02-21 12:12:45 +0100

    a siple log

--- WebApplicationTest/WebApplicationTest/ClientApp/src/app/counter/counter.component.ts
+++ WebApplicationTest/WebApplicationTest/ClientApp/src/app/counter/counter.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-counter-component',
   templateUrl: './counter.component.html'
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
   public currentCount = 0;
+
+  ngOnInit() {
+    console.log("other dev changes")
+  }
 
   public incrementCounter() {
     this.currentCount++;
